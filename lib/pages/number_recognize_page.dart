897c725a98e28bbeb57b639c0e956255cf14dab3
TatsukiIshijima <tatsuki445@gmail.com>
2022-04-02 18:02:04 +0900

    add text detector

--- lib/pages/number_recognize_page.dart
+++ lib/pages/number_recognize_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:nengar/widgets/camera_view.dart';
 
 class NumberRecognizePage extends StatefulWidget {
@@ -12,18 +13,37 @@ class NumberRecognizePage extends StatefulWidget {
 }
 
 class _NumberRecognizeState extends State<NumberRecognizePage> {
-  // late CustomPaint customPaint;
+  TextDetectorV2 textDetector = GoogleMlKit.vision.textDetectorV2();
+  bool isBusy = false;
 
   @override
-  void dispose() {
+  void dispose() async {
     super.dispose();
+    await textDetector.close();
   }
 
   @override
   Widget build(BuildContext context) {
     return CameraView(
-        title: 'Text Detector',
-        // customPaint: customPaint,
-        onImage: (inputImage) {});
+      title: 'Text Detector',
+      onImage: (inputImage) {
+        processImage(inputImage);
+      },
+    );
+  }
+
+  Future<void> processImage(InputImage inputImage) async {
+    if (isBusy) return;
+    isBusy = true;
+    final recognisedText = await textDetector.processImage(
+      inputImage,
+      script: TextRecognitionOptions.DEVANAGIRI,
+    );
+    print('Found: ${recognisedText.blocks.length} textBlocks, '
+        '${recognisedText.text} resText');
+    isBusy = false;
+    if (mounted) {
+      setState(() {});
+    }
   }
 }
