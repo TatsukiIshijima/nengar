363f4807d446b75f48f52a5f4436963d020640e3
TatsukiIshijima <tatsuki445@gmail.com>
2022-04-30 10:25:36 +0900

    add model

---
+++ lib/model/WinningNumbers.dart
@@ -0,0 +1,8 @@
+class WinningNumbers {
+  WinningNumbers(
+      this.firstWinNumber, this.secondWinNumber, this.thirdWinNumber);
+
+  final String firstWinNumber;
+  final String secondWinNumber;
+  final String thirdWinNumber;
+}
