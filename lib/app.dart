70c1f3e9a77534cd332301c5833d501af8647c8a
TatsukiIshijima <tatsuki445@gmail.com>
2022-05-01 23:32:31 +0900

    apply router

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,59 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_use/flutter_use.dart';
+import 'package:go_router/go_router.dart';
+import 'package:nengar/datasource/numbers_datasouce.dart';
+import 'package:nengar/datasource/numbers_datasource_impl.dart';
+import 'package:nengar/pages/number_edit_page.dart';
 import 'package:nengar/pages/number_recognize_page.dart';
+import 'package:nengar/pages/splash_page.dart';
+import 'package:nengar/router/app_router.dart';
+import 'package:nengar/router/app_router_impl.dart';
 
-class App extends StatelessWidget {
+class App extends HookWidget {
+  App({Key? key}) : super(key: key);
+
+  late AppRouter _appRouter;
+  late NumbersDataSource _numbersDataSource;
+
   @override
   Widget build(BuildContext context) {
+    useEffectOnce(() {
+      _appRouter = AppRouterImpl();
+      _numbersDataSource = NumbersDataSourceImpl();
+    });
+
     const env = String.fromEnvironment('FLAVOR');
+
+    final _router = GoRouter(routes: [
+      GoRoute(
+        path: AppRouter.splashPageRoutePath,
+        builder: (context, state) => SplashPage(
+          appRouter: _appRouter,
+          numbersDataSource: _numbersDataSource,
+        ),
+      ),
+      GoRoute(
+        path: AppRouter.numberEditPageRoutePath,
+        builder: (context, state) => NumberEditPage(),
+      ),
+      GoRoute(
+        path: AppRouter.numberRecognizePageRoutePath,
+        builder: (context, state) => NumberRecognizePage(),
+      ),
+    ]);
+
     return MaterialApp(
       title: env,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: _flavorBanner(
-        child: NumberRecognizePage(),
+        child: MaterialApp.router(
+          routeInformationParser: _router.routeInformationParser,
+          routerDelegate: _router.routerDelegate,
+        ),
         show: kDebugMode,
       ),
     );
@@ -25,18 +66,18 @@ class App extends StatelessWidget {
     const env = String.fromEnvironment('FLAVOR');
     return show
         ? Banner(
-            child: child,
-            location: BannerLocation.topStart,
-            message: env,
-            color: Colors.green.withOpacity(0.6),
-            textStyle: const TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 12.0,
-                letterSpacing: 1.0),
-            textDirection: TextDirection.ltr,
-          )
+      child: child,
+      location: BannerLocation.topStart,
+      message: env,
+      color: Colors.green.withOpacity(0.6),
+      textStyle: const TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 12.0,
+          letterSpacing: 1.0),
+      textDirection: TextDirection.ltr,
+    )
         : Container(
-            child: child,
-          );
+      child: child,
+    );
   }
 }
--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,15 +1,47 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:async';
 
-class SplashPage extends StatelessWidget {
-  const SplashPage({Key? key}) : super(key: key);
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_use/flutter_use.dart';
+import 'package:nengar/datasource/numbers_datasouce.dart';
+import 'package:nengar/router/app_router.dart';
+
+class SplashPage extends HookWidget {
+  const SplashPage({
+    Key? key,
+    required this.appRouter,
+    required this.numbersDataSource,
+  }) : super(key: key);
+
+  final AppRouter appRouter;
+  final NumbersDataSource numbersDataSource;
+
+  void initialize(BuildContext context) {
+    Timer(
+      const Duration(milliseconds: 2000),
+      () async {
+        final numbersData = await numbersDataSource.load();
+        if (numbersData == null) {
+          appRouter.goEditPage(context);
+        } else {
+          appRouter.goRecognizePage(context);
+        }
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     const env = String.fromEnvironment('FLAVOR');
-    return CupertinoPageScaffold(
+
+    useEffectOnce(() {
+      initialize(context);
+    });
+
+    return const CupertinoPageScaffold(
       child: Center(
         child: Text(
-          'Splash ${env}',
+          'Splash $env',
         ),
       ),
     );
