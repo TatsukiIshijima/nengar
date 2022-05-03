d4d917af173bd0255c3ab02f00fec7137700d352
TatsukiIshijima <tatsuki445@gmail.com>
2022-05-03 22:11:48 +0900

    fix

--- lib/model/numbers_data.g.dart
+++ lib/model/numbers_data.g.dart
@@ -7,12 +7,12 @@ part of 'numbers_data.dart';
 // **************************************************************************
 
 NumbersData _$NumbersDataFromJson(Map<String, dynamic> json) => NumbersData(
-      WinNumbers.fromJson(json['winningNumbers'] as Map<String, dynamic>),
+      WinNumbers.fromJson(json['winNumbers'] as Map<String, dynamic>),
       DateTime.parse(json['savedDateTime'] as String),
     );
 
 Map<String, dynamic> _$NumbersDataToJson(NumbersData instance) =>
     <String, dynamic>{
-      'winningNumbers': instance.winNumbers,
+      'winNumbers': instance.winNumbers,
       'savedDateTime': instance.savedDateTime.toIso8601String(),
     };
