82660f28bffcd202e29ae01fc70de53c3d6fbee1
TatsukiIshijima <tatsuki445@gmail.com>
2023-01-05 23:30:00 +0900

    create NumberLoadViewModel and refactor NumberRecognizeViewModel
    Signed-off-by: TatsukiIshijima <tatsuki445@gmail.com>

--- lib/viewmodel/number_recognize_viewmodel.dart
+++ lib/viewmodel/number_recognize_viewmodel.dart
@@ -1,30 +1,41 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_use/flutter_use.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:nengar/extension/RecognisedTextExtension.dart';
 import 'package:nengar/model/recognized_text.dart' as model;
-import 'package:nengar/model/uimodel/win_numbers_uimodel.dart';
 import 'package:nengar/model/uimodel/win_result_uimodel.dart';
 import 'package:nengar/repository/numbers_repository.dart';
 import 'package:nengar/usecase/judge_numbers_usecase.dart';
-import 'package:nengar/usecase/load_numbers_usecase.dart';
+import 'package:nengar/widgets/number_detector_painter.dart';
 
 class NumberRecognizeViewModel {
   late final NumbersRepository _numbersRepository;
-  late final LoadNumbersUseCase _loadNumbersUseCase;
   late final JudgeNumbersUseCase _judgeNumbersUseCase;
 
   NumberRecognizeViewModel(NumbersRepository numbersRepository) {
     _numbersRepository = numbersRepository;
-    _loadNumbersUseCase = LoadNumbersUseCase(_numbersRepository);
     _judgeNumbersUseCase = JudgeNumbersUseCase(_numbersRepository);
   }
 
-  final ValueNotifier<bool> _forceUpdateUseState = useState(false);
+  final TextRecognizer _textRecognizer = TextRecognizer();
 
-  bool get forceUpdate => _forceUpdateUseState.value;
+  final ValueNotifier<bool> _canProcessUseState = useState(true);
+
+  final ValueNotifier<bool> _isBusyUseState = useState(false);
+
+  final ValueNotifier<InputImage?> _inputImageUseState = useState(null);
+
+  set inputImage(value) {
+    _inputImageUseState.value = value;
+  }
+
+  final ValueNotifier<CustomPaint?> _customPaintUseState = useState(null);
+
+  CustomPaint? get customPaint => _customPaintUseState.value;
 
   final ValueNotifier<bool> _isEditModeUseState = useState(false);
-
-  bool get isEditMode => _isEditModeUseState.value;
 
   set isEditMode(value) {
     _isEditModeUseState.value = value;
@@ -35,31 +46,58 @@ class NumberRecognizeViewModel {
 
   WinResultUiModel get winResultUiModel => _winResultUseState.value;
 
-  final ValueNotifier<WinNumbersUiModel> _winNumbersUseState =
-      useState(WinNumbersUiModel.empty());
-
-  WinNumbersUiModel get winNumbersUiModel => _winNumbersUseState.value;
-
-  void onBuild(bool forceUpdate) {
-    _changeForceUpdateFlag(forceUpdate);
-
-    useEffect(() {
-      _onLoadNumbers();
-      return;
-    }, [_forceUpdateUseState.value]);
-  }
-
-  void _changeForceUpdateFlag(bool isOn) {
-    _forceUpdateUseState.value = isOn;
-  }
-
-  void _onLoadNumbers() {
-    _loadNumbersUseCase.execute().then((uiModel) {
-      _winNumbersUseState.value = uiModel;
+  void onBuild() {
+    useEffectOnce(() {
+      _canProcessUseState.value = true;
+      return () async {
+        _canProcessUseState.value = false;
+        await _textRecognizer.close();
+      };
     });
+
+    useValueChanged<InputImage?, Future<InputImage?>>(
+      _inputImageUseState.value,
+      (oldValue, oldResult) async {
+        final inputImage = _inputImageUseState.value;
+        if (inputImage == null) {
+          return;
+        }
+        await _onRecognizeProcess(inputImage);
+      },
+    );
   }
 
-  Future onRecognize(model.RecognizedText recognizedText) async {
+  Future _onRecognizeProcess(InputImage inputImage) async {
+    if (_isEditModeUseState.value) return;
+    if (!_canProcessUseState.value) return;
+    if (_isBusyUseState.value) return;
+
+    _isBusyUseState.value = true;
+    final recognisedText = await _textRecognizer.processImage(inputImage);
+    final recognizedText = recognisedText.toRecognizedText().filteredByNumber();
+    _paintRecognizedResultIfNeed(
+      inputImage,
+      recognizedText,
+    );
+    await _onJudgeNumberFrom(recognizedText);
+    _isBusyUseState.value = false;
+  }
+
+  void _paintRecognizedResultIfNeed(
+    InputImage inputImage,
+    model.RecognizedText recognizedText,
+  ) {
+    final size = inputImage.inputImageData?.size;
+    final rotation = inputImage.inputImageData?.imageRotation;
+    if (size != null && rotation != null) {
+      final painter = NumberDetectorPainter(recognizedText, size, rotation);
+      _customPaintUseState.value = CustomPaint(painter: painter);
+    } else {
+      _customPaintUseState.value = null;
+    }
+  }
+
+  Future _onJudgeNumberFrom(model.RecognizedText recognizedText) async {
     final winType = await _judgeNumbersUseCase.execute(recognizedText);
     _winResultUseState.value = WinResultUiModel.from(winType);
   }
