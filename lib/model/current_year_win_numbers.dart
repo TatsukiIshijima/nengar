eb72c83c6d5999e9d1b87d516254e5ebaf9f5ce5
TatsukiIshijima <tatsuki445@gmail.com>
2023-01-12 22:55:54 +0900

    create const values
    Signed-off-by: TatsukiIshijima <tatsuki445@gmail.com>

---
+++ lib/model/current_year_win_numbers.dart
@@ -0,0 +1,7 @@
+class CurrentYearWinNumbers {
+  static const String first = '123456';
+  static const String second = '7890';
+  static const String thirdPrimary = '13';
+  static const String thirdSecondary = '57';
+  static const String thirdTertiary = '91';
+}
