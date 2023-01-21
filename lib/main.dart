46cea252c3f89af43e41232dc7500d8bd05ad534
TatsukiIshijima <tatsuki445@gmail.com>
2023-01-21 15:19:39 +0900

    add package info plus
    Signed-off-by: TatsukiIshijima <tatsuki445@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/widgets.dart';
 import 'package:nengar/app.dart';
+import 'package:nengar/log.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 List<CameraDescription> cameras = [];
 
 Future<void> run() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+
+  String appName = packageInfo.appName;
+  String packageName = packageInfo.packageName;
+  String version = packageInfo.version;
+  String buildNumber = packageInfo.buildNumber;
+
+  Log.logger.d(
+      "PackageInfo: appName=$appName, packageName=$packageName, version=$version, buildNumber=$buildNumber");
 
   cameras = await availableCameras();
 
